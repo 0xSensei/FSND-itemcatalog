343692af505333dd433cb58395116c00f6d9c90b
0xSensei <44272893+0xSensei@users.noreply.github.com>
2018-11-24 18:05:25 +0300

    Update database_populate.py

--- database_populate.py
+++ database_populate.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Catalog, Base, CatalogItem, User
 
-engine = create_engine('sqllite:///catalog.db')
+engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
